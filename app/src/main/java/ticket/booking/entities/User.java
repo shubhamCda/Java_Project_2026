18cf7130aad350893a3441dfc951aab5bf8b0dd8
shubhamCda <shubhambodalkar281@gmail.com>
2026-08-21 03:09:42 +0530

    Add user data json file

--- app/src/main/java/ticket/booking/entities/User.java
+++ app/src/main/java/ticket/booking/entities/User.java
@@ -7,7 +7,6 @@ public class User {
     private String name;
     private String password;
     private String hashPassword;
-
     private List<Ticket> ticketsBooked;
     private  String userId;
 }

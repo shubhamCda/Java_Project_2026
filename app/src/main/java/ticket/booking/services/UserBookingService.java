d03179016d059f08aa97d38229379a1b272b410a
shubhamCda <shubhambodalkar281@gmail.com>
2026-08-21 03:12:51 +0530

    Add user booking service file

--- app/src/main/java/ticket/booking/services/UserBookingService.java
+++ app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -1,4 +1,20 @@
 package ticket.booking.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ticket.booking.entities.User;
+
+import java.io.File;
+
 public class UserBookingService {
+
+    private User user;
+    
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final String USERS_PATH = "../localDb/users.json";
+
+    public UserBookingService(User user1){
+        this.user = user1;
+        File users = new File(USERS_PATH);
+    }
 }

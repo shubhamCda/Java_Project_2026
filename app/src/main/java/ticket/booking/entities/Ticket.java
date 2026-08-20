a153c97caf8c3b48f98ffa64925b016ae72799f4
shubhamCda <shubhambodalkar281@gmail.com>
2026-08-21 01:36:14 +0530

    Add ticket requirements in entities package

--- app/src/main/java/ticket/booking/entities/Ticket.java
+++ app/src/main/java/ticket/booking/entities/Ticket.java
@@ -1,4 +1,12 @@
 package ticket.booking.entities;
 
 public class Ticket {
+
+    private String ticketId;
+    private String userId;
+    private String source;
+    private String destination;
+    private Train train;
+
+
 }

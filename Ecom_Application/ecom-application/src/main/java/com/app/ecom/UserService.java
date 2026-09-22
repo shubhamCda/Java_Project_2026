bebebb8ed5ffe400ac51a78d0aec047ff3b11b98
shubhamCda <shubhambodalkar281@gmail.com>
2026-09-22 06:14:25 +0530

    Add logic to increment user Id

--- Ecom_Application/ecom-application/src/main/java/com/app/ecom/UserService.java
+++ Ecom_Application/ecom-application/src/main/java/com/app/ecom/UserService.java
@@ -8,12 +8,14 @@ import java.util.List;
 @Service
 public class UserService {
     private List<User> userList = new ArrayList<>();
+    private Long nextId = 1L;
 
     public List<User> fetchAllUsers(){
         return userList;
     }
 
     public List<User> addUser(User user){
+        user.setId(nextId++);
         userList.add(user);
         return userList;
     }

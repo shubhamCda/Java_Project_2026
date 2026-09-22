457358ba45f7e23833693fe6ecde1f585fd1c70d
shubhamCda <shubhambodalkar281@gmail.com>
2026-09-22 06:05:25 +0530

    Add @RequiredArgsConstructor to automatically create constructor for final fields

--- Ecom_Application/ecom-application/src/main/java/com/app/ecom/UserController.java
+++ Ecom_Application/ecom-application/src/main/java/com/app/ecom/UserController.java
@@ -1,25 +1,29 @@
 package com.app.ecom;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class UserController {
-    private List<User> userList = new ArrayList<>();
+
+    private final UserService userService;
+
+    //    private List<User> userList = new ArrayList<>();
 
     @GetMapping("/api/users")
     public List<User> getAllUsers(){
-        return userList;
+        return userService.fetchAllUsers();
     }
 
     @PostMapping("/api/users")
-    public List<User> createUsers(@RequestBody User user){
-        userList.add(user);
-        return userList;
+    public String  createUsers(@RequestBody User user){
+        userService.addUser(user);
+        return "User added";
     }
 }
